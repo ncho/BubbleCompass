3526abfe8a973f679b24d94994d6c53edab930b1
Nathan Cho <ncho@esri.com>
2025-12-23 14:16:21 -0800

    Initial Commit

---
+++ BubbleCompass/BubbleCompassApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubbleCompassApp.swift
+//  BubbleCompass
+//
+//  Created by Nathan Cho on 12/23/25.
+//
+
+import SwiftUI
+
+@main
+struct BubbleCompassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BubbleCompassTests/BubbleCompassTests.swift
@@ -0,0 +1,17 @@
+//
+//  BubbleCompassTests.swift
+//  BubbleCompassTests
+//
+//  Created by Nathan Cho on 12/23/25.
+//
+
+import Testing
+@testable import BubbleCompass
+
+struct BubbleCompassTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
